27a0a9aa0d860324c9aef3adc38f4dcfc134d1f8
lymarrie <lucmarrie1@gmail.com>
2023-02-07 10:40:22 -0500

    more components

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -16,7 +16,7 @@ const Divider = (props: DividerProps) => {
 
   return (
     <>
-        <div className="relative py-5">
+        <div className="relative py-5 bg-white">
             <div className="absolute inset-0 flex items-center" aria-hidden="true">
             <div className="w-full border-t border-gray-500" style={style} /></div>
             <div className="relative flex justify-center">
