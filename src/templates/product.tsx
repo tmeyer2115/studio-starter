9bde9d1cee53d74c89923e52cde787691540a0ef
Andrew Sanchez <asanchez@yext.com>
2023-03-03 13:58:22 -0500

    add in placeholder text

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -23,7 +23,7 @@ export const config: TemplateConfig = {
     $id: "skis",
     filter: { entityTypes: ["ce_skis"] },
     localization: { locales: ["en"], primary: false },
-    fields: ["name", "c_price", "description", "photoGallery", "slug"],
+    fields: ["c_price", "photoGallery", "slug"],
   },
 };
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
@@ -43,7 +43,7 @@ export default function Product({ document }: TemplateProps) {
             <GridContainer>
               <VStack>
                 <Headline
-                  value={document.name}
+                  value={`Ski Name - Add Here`}
                   fontWeight="bold"
                   textSize="4xl"
                 />
@@ -52,7 +52,7 @@ export default function Product({ document }: TemplateProps) {
                   <Reviews averageRating={5} reviewCount={1995} />
                 </HStack>
                 <Paragraph
-                  value={`${document.description}`}
+                  value={`Add Description Here`}
                   fontWeight=""
                   textSize=""
                 />
