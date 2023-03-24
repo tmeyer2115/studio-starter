704bd55eb9773a95d1e71e37c65f9f4acc769f6e
Andrew Sanchez <asanchez@yext.com>
2023-03-24 16:02:11 -0400

    add prestart script

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -11,17 +11,17 @@ const Divider = (props: DividerProps) => {
 
   const style = {
     borderColor: `${color}`,
-    borderWidth: `${width}`
-  }
+    borderWidth: `${width}`,
+  };
 
   return (
     <>
-        <div className="relative py-5 bg-white">
-            <div className="absolute inset-0 flex items-center" aria-hidden="true">
-            <div className="w-full border-t border-gray-500" style={style} /></div>
-            <div className="relative flex justify-center">
-            </div>
+      <div className="relative py-5 bg-white">
+        <div className="absolute inset-0 flex items-center" aria-hidden="true">
+          <div className="w-full border-t border-gray-500" style={style} />
         </div>
+        <div className="relative flex justify-center"></div>
+      </div>
     </>
   );
 };
