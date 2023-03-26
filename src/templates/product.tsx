59009bddde8ae3b3a573a0c7db57dd16f4d31ebb
Andrew Sanchez <asanchez@yext.com>
2023-03-26 09:50:58 -0400

    Yext Studio Commit

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -13,6 +13,7 @@ import CenteredContainer from "../components/CenteredContainer";
 import PageLayout from "../components/PageLayout";
 import Paragraph from "../components/Paragraph";
 import ProductImage from "../components/ProductImage";
+import ProductTable from "../components/ProductTable";
 import Reviews from "../components/Reviews";
 import VStack from "../components/VStack";
 import "../index.css";
@@ -22,7 +23,7 @@ export const config: TemplateConfig = {
     $id: "skis",
     filter: { entityTypes: ["ce_skis"] },
     localization: { locales: ["en"], primary: false },
-    fields: ["name", "c_price", "photoGallery", "slug"],
+    fields: ["name", "c_price", "description", "photoGallery", "slug"],
   },
 };
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
@@ -51,7 +52,7 @@ export default function Product({ document }: TemplateProps) {
                   <Reviews averageRating={5} reviewCount={1995} />
                 </HStack>
                 <Paragraph
-                  value={`Add Description Here`}
+                  value={`${document.description}`}
                   fontWeight=""
                   textSize=""
                 />
@@ -83,6 +84,15 @@ export default function Product({ document }: TemplateProps) {
                 price={799.99}
               />
             </ItemsGrid>
+            <ProductTable
+              title="Product Details"
+              rockerType="All Mountain Rocker – Smooth rocker at both tip and tail with camber underfoot provides balanced performance anywhere on the mountain."
+              shape="True Tip LT – By trimming the amount of heavy ABS plastic in the tip and extending the lightweight wood core into this area with a taper that further reduces weight, Nordica enhances playfulness and maneuverability of the ski without compromising that unmatchable feeling of powerful and control."
+              core="Lite Performance Wood | Poplar / Beech"
+              laminates="Carbon Chassis LT – A proprietary blend of carbon materials to reduce weight while maximizing performance."
+              sidewalls="ABS Sidewalls"
+              bindingCompatibility="We recommend a brake width equal to or at most 15 mm wider than the ski waist width."
+            />
           </CenteredContainer>
         </PageLayout>
       </>
